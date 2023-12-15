322dde7c8346821a1887f7fc4b9412ae8260a064
XGamer952 <xavier.dargan@verizon.net>
2023-12-15 06:43:26 -0800

    feat: Front-end now calls SpotifyAPI to print songs

--- frontend/src/components/TrackDisplay.jsx
+++ frontend/src/components/TrackDisplay.jsx
@@ -1,31 +1,30 @@
-import React, {useState} from 'react'
-import SongBox from './SongBox'
-import InputBar from './InputBar';
+import React, { useState } from "react";
+import SongBox from "./SongBox";
+import InputBar from "./InputBar";
 
 const TrackDisplay = () => {
+  const [songs, setSongs] = useState([]);
 
-    const [songs, setSongs] = useState([]);
+  const addSong = (newSong) => {
+    setSongs([...songs, newSong]);
+  };
 
-    const addSong = (newSong) => {
-        setSongs([...songs, newSong]);
-    };
+  return (
+    <div className="flex flex-col overflow-scroll">
+      <div className="overflow-scroll bg-beige border-dksage border-2 w-[500px] h-[450px] ">
+        {songs.map((song, index) => (
+          <SongBox
+            key={index}
+            url={song.album.images[0].url}
+            title={song.name}
+            artist={song.artists[0].name}
+            color="red-600"
+          />
+        ))}
+      </div>
+      <InputBar onAddSong={addSong} />
+    </div>
+  );
+};
 
-    return (
-        <div className='flex flex-col overflow-scroll'>
-            <div className="overflow-scroll bg-beige border-dksage border-2 w-[500px] h-[450px] ">
-                {songs.map((song, index) => (
-                <SongBox
-                    key={index}
-                    url={require("../image/clairo.png")}
-                    title={song.title}
-                    artist={song.artist}
-                    color="red-600"
-                />
-                ))}
-            </div>
-            <InputBar onAddSong={addSong} />
-        </div>
-      );
-    };
-    
-    export default TrackDisplay;
+export default TrackDisplay;
