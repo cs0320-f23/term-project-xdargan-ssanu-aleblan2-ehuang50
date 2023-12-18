f581a96ccc47832576192aa8b789db32507b7273
emmabh99 <emma.b.huang@gmail.com>
2023-12-18 02:16:55 -0500

    Accomodating for jest

--- frontend/tests/unit.test.ts
+++ frontend/tests/unit.test.ts
@@ -2,3 +2,25 @@
 
 // Track Display Tests
 
+// TrackDisplay.test.js
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { TrackDisplay } from './TrackDisplay';
+
+describe('addSong function', () => {
+  test('should add a new song to the list', () => {
+    render(<TrackDisplay />);
+
+    // Simulate adding a new song
+    userEvent.type(screen.getByPlaceholderText(/enter song title/i), 'New Song');
+    userEvent.type(screen.getByPlaceholderText(/enter artist/i), 'New Artist');
+    userEvent.click(screen.getByText(/add song/i));
+
+    // Verify that the new song is added to the list
+    expect(screen.getByText(/New Song/i)).toBeInTheDocument();
+    expect(screen.getByText(/New Artist/i)).toBeInTheDocument();
+  });
+
+  // Add more test cases as needed
+});
