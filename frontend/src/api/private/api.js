773c6cc3dab1d446739491fbadc2996280ad3339
XGamer952 <xavier.dargan@verizon.net>
2023-12-19 09:09:27 -0800

    refactor: Removed access key for privacy reasons.

--- frontend/src/api/private/api.js
+++ frontend/src/api/private/api.js
@@ -1,2 +1,1 @@
-export const ACCESS_TOKEN =
-  "BQClwqjOxqJkAPyivfQhHDUWozPYcfbBa-bmfSqsuaAwfGBIe3c-uhZcI2pNSbfGugBR6nE5bzSYFoPcrUY5xjuN8fF4Mp34i1YUajBwSMdx3_6jl3k";
+export const ACCESS_TOKEN = "insert access key here";
