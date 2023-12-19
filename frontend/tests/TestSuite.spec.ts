b68e557c1d52dcd6cc1383b2de5eb3ca330827ab
emmabh99 <emma.b.huang@gmail.com>
2023-12-19 12:41:52 -0500

    Test suite done

--- frontend/tests/TestSuite.spec.ts
+++ frontend/tests/TestSuite.spec.ts
@@ -41,11 +41,11 @@ test('should have correct text content in Stats component', async ({ page }) => 
  * In summary, we have tests for:
  * - Entering an existing song, with complete info
  * - Entering multiple existing songs
- * - Entering a song that doesn't exist
- * - Entering a song with only title (incomplete info)
- * - Entering a song with only artist (incomplete info)
- * - Entering a song with typos in information
- * - Submitting empty input
+ * - Entering a song that doesn't exist (ERROR HANDLING)
+ * - Entering a song with only title (incomplete info - ERROR HANDLING)
+ * - Entering a song with only artist (incomplete info - ERROR HANDLING)
+ * - Entering a song with typos in information (ERROR HANDLING)
+ * - Submitting empty input (ERRO HANDLING)
  * - Entering a song then removing it (and then adding another one)
  * - Entering the same song twice - this is a bug
  * - Entering song that has multiple versions available (i.e. instrumental)
@@ -383,6 +383,119 @@ test("Removing all songs after recommendations generated", async ({ page }) => {
 - Generating recommendations with all songs removed 
 */
 
+test('Generating recommendations with no songs entered', async ({ page }) => {
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+
+  // Add no songs
+
+  // Generate recommendations
+  await page.click(generateRecommendationsButtonSelector);
+
+  // Wait for recommendations to be generated
+  const recommendationsSelector = '.recommendations';
+  await page.waitForSelector(recommendationsSelector);
+
+  // Verify that no recommendations are displayed
+  const recommendationItemSelector = '.recommendation-item';
+  await page.waitForSelector(recommendationItemSelector);
+
+  const recommendationsCount = await page.$$eval(recommendationItemSelector, items => items.length);
+  expect(recommendationsCount).equals(0);
+});
+
+test('Generating recommendations with a 1, 2, 5, and 10 songs', async ({ page }) => {
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+
+  // Add a few songs
+  await page.fill(inputTitleSelector, 'Song 1');
+  await page.fill(inputArtistSelector, 'Artist 1');
+  await page.click(addSongButtonSelector);
+
+  await page.fill(inputTitleSelector, 'Song 2');
+  await page.fill(inputArtistSelector, 'Artist 2');
+  await page.click(addSongButtonSelector);
+
+  // Generate recommendations
+  await page.click(generateRecommendationsButtonSelector);
+
+  // Wait for recommendations to be generated
+  const recommendationsSelector = '.recommendations';
+  await page.waitForSelector(recommendationsSelector);
+
+  // Verify that recommendations are displayed
+  const recommendationItemSelector = '.recommendation-item';
+  await page.waitForSelector(recommendationItemSelector);
+  const recommendationsCount = await page.$$eval(recommendationItemSelector, items => items.length);
+  expect(recommendationsCount).toBeGreaterThanOrEqual(5);
+});
+
+test('Generating recommendations after removing song', async ({ page }) => {
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const removeSongButtonSelector = 'button:has-text("Remove Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+
+  // Add a song
+  await page.fill(inputTitleSelector, 'Song Title');
+  await page.fill(inputArtistSelector, 'Artist');
+  await page.click(addSongButtonSelector);
+
+  // Generate recommendations
+  await page.click(generateRecommendationsButtonSelector);
+
+  // Wait for recommendations to be generated 
+  const recommendationsSelector = '.recommendations';
+  await page.waitForSelector(recommendationsSelector);
+
+  // Remove a song from recommendations
+  await page.click(removeSongButtonSelector);
+
+  // Verify that the song is removed from recommendations
+  const removedSongSelector = '.removed-song';
+  await page.waitForSelector(removedSongSelector, { state: 'hidden' });
+
+});
+
+test('Generating recommendations after two/all songs', async ({ page }) => {
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const removeSongButtonSelector = 'button:has-text("Remove Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+
+  // Add songs
+  await page.fill(inputTitleSelector, 'Song Title');
+  await page.fill(inputArtistSelector, 'Artist');
+  await page.click(addSongButtonSelector);
+  await page.fill(inputTitleSelector, 'Song Title 2');
+  await page.fill(inputArtistSelector, 'Artist 2');
+  await page.click(addSongButtonSelector);
+
+  // Generate recommendations
+  await page.click(generateRecommendationsButtonSelector);
+
+  // Wait for recommendations to be generated 
+  const recommendationsSelector = '.recommendations';
+  await page.waitForSelector(recommendationsSelector);
+
+  // Remove songs
+  await page.click(removeSongButtonSelector);
+  await page.click(removeSongButtonSelector);
+  
+
+  // Verify that the song is removed from recommendations
+  const removedSongSelector = '.removed-song';
+  await page.waitForSelector(removedSongSelector, { state: 'hidden' });
+
+});
+
  
 /* Graph
 - Checking if data point corresponds accurately (placement)
@@ -392,49 +505,172 @@ test("Removing all songs after recommendations generated", async ({ page }) => {
 - Checking that all combinations of labels are available and reflected
 */
 
-/* Funcationality so far:
-Loading Song, Song Displays in List, Corresponding Data Point (for plot) is created with song
-*/ 
+test('Graph rendering and interaction', async ({ page }) => {
 
-// State Change tests - applicable, and how so?
-// Loading multiple songs in, and checking accuracy
-// Checking if data point corresponds accurately
-// Checking if data point corresponds accurately after loading multiple songs
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+  const removeSongButtonSelector = 'button:has-text("Remove Song")';
+  const graphSelector = 'svg';
 
-// Test for searching/locating song by data point
-// Test for searching/locating data point by song name
-// Test for searching/locating data point by Artist
-// (partially covered) Entering song that doesn't exist (trying to get empty response)
-// Entering song of same name
-// Enteirng song of same artist, twice
-// Entering song of same ariist, with break in between
-// Entering song name just name, no Artist name
-// Entering song just Artist, no name
-test("load then view", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-  await page.fill(
-    "input[aria-label='Command input']",
-    "load_file starData.csv"
-  );
-  await page.click("button:has-text('Submit')");
-  await page.fill("input[aria-label='Command input']", "view");
-  await page.click("button:has-text('Submit')");
-  const tableElement = await page.$("table.tablecenter");
-  expect(tableElement).not.toBeNull();
+  // Add data for the graph
+  const graphData = [
+    { x: 20, y: 30 },
+    { x: 40, y: 60 }
+  ];
+
+  // Add color data for each point
+  const colorData = ['#ff0000', '#00ff00', '#0000ff']; // Colors for each data point
+
+  // Check if the point is displayed
+  await page.hover('#point-0');
+  const tooltipSelector = '#tooltip';
+  await page.waitForSelector(tooltipSelector);
+
+  // Verify that the point contains the correct information
+  const tooltipText = await page.textContent(tooltipSelector);
+  expect(tooltipText).toContain('(20, 30)');
+  await page.mouseout('#point-0');
+
+  // REMOVE A SONG
+  await page.click(removeSongButtonSelector);
+  await page.waitForSelector(graphSelector);
+  const initialPointCount = await page.$$eval('circle', circles => circles.length);
+
+  // Verify that the corresponding point is removed from the graph
+  const updatedPointCount = await page.$$eval('circle', circles => circles.length);
+  expect(updatedPointCount).toBe(initialPointCount - 1);
+
+  // Verify that the point is removed
+  await page.waitForSelector(tooltipSelector, { state: 'hidden' });
 });
 
+
 /** 
- * Integration Testing (to backend API) 
+ * Integration Testing (to backend API) - testing spotifyApi.js
  */
+
+// Mock ACCESS_TOKEN for testing
+const ACCESS_TOKEN = 'your_mocked_access_token';
+
+// Mock the fetch function to control responses in tests
+const mockFetch = async (url, options) => {
+  if (url.includes('search')) {
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        tracks: {
+          items: [
+            { id: 'trackId1' },
+          ]
+        }
+      })
+    });
+  } else if (url.includes('audio-features')) {
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        // Mocked audio features
+        danceability: 0.8,
+        energy: 0.9,
+      })
+    });
+  } else if (url.includes('recommendations')) {
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        // Mocked recommendations
+        tracks: [
+          { id: 'recommendedTrackId1' },
+        ]
+      })
+    });
+  }
+  // Default response
+  return Promise.resolve({
+    json: () => Promise.resolve({})
+  });
+};
+
+test('getTrackInfo returns track information', async () => {
+  const trackInfo = await getTrackInfo('SongName', 'ArtistName');
+  expect(trackInfo).toBeDefined();
+  expect(trackInfo.id).toBe('trackId1');
+});
+
+test('getAudioFeatures returns audio features', async () => {
+  const audioFeatures = await getAudioFeatures('trackId1');
+  expect(audioFeatures).toBeDefined();
+  expect(audioFeatures.danceability).toBe(0.8);
+  // Add more assertions based on the expected behavior of getAudioFeatures
+});
+
+test('getAudioFeaturesFromSearch returns audio features from search', async () => {
+  const audioFeatures = await getAudioFeaturesFromSearch('SongName', 'ArtistName');
+  expect(audioFeatures).toBeDefined();
+  expect(audioFeatures.danceability).toBe(0.8);
+  // Add more assertions based on the expected behavior of getAudioFeaturesFromSearch
+});
+
+test('getRecommendations returns recommended tracks', async () => {
+  const recommendations = await getRecommendations([{ id: 'trackId1' }]);
+  expect(recommendations).toBeDefined();
+  expect(recommendations.tracks[0].id).toBe('recommendedTrackId1');
+  // Add more assertions based on the expected behavior of getRecommendations
+});
+
+
 
 /**
- * Error Handling
+ * Random and Fuzz Testing Testing - INPUT BAR
+ * Decided to conduct random testing on input bar to explore how our interface handles a breadth of input, and stress test.
+ * Graph would get overcrowded
  */
 
-/**
- * Random and Fuzz Testing Testing
- */
+const getTrackInfo = Promise.resolve({ name: 'Mocked Track', album: { images: [{ url: 'mocked-url' }] } });
+const removeSong = useState([]);
+const generateRecommendations = useState([]);
+const onAddSong = useState([]);
 
+test('Randoming testing for input bar', async ({ page }) => {
+
+  // Your selectors might be different based on your actual implementation
+  const inputTitleSelector = 'input[placeholder="Enter song title..."]';
+  const inputArtistSelector = 'input[placeholder="Enter artist..."]';
+  const addSongButtonSelector = 'button:has-text("Add Song")';
+  const removeSongButtonSelector = 'button:has-text("Remove Song")';
+  const generateRecommendationsButtonSelector = 'button:has-text("Generate Recommendations")';
+
+  // Simulate random user interactions
+  for (let i = 0; i < 100; i++) {
+    const randomTitle = generateRandomString(10);
+    const randomArtist = generateRandomString(10);
+
+    // Fill input fields with random values
+    await page.fill(inputTitleSelector, randomTitle);
+    await page.fill(inputArtistSelector, randomArtist);
+    await page.click(addSongButtonSelector);
+
+    // Validate that the onAddSong function was called with the expected parameters
+    expect(onAddSong).toHaveBeenCalledWith(expect.objectContaining({ name: 'Mocked Track' }));
+
+    // Simulate clicking the "Remove Song" button, validate
+    await page.click(removeSongButtonSelector);
+    expect(removeSong).toHaveBeenCalled();
+
+    // Simulate clicking the "Generate Recommendations" button, validate
+    await page.click(generateRecommendationsButtonSelector);
+    expect(generateRecommendations).toHaveBeenCalled();
+  }
+});
+
+// Generating random string
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 
 /* ACCESSIBILITY TESTS (US3)
