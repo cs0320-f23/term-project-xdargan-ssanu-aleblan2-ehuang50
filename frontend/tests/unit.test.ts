600ff0774c0a6a7677434b3b0c5fb919cb8e4574
emmabh99 <emma.b.huang@gmail.com>
2023-12-19 12:50:21 -0500

    feat more documentation

--- frontend/tests/unit.test.ts
+++ frontend/tests/unit.test.ts
@@ -1,8 +1,13 @@
 /**
- * Unit Testing - Unit testing covered functions
+ * Unit Testing - Unit testing on functions in InputBar, SongBox, and Stats
+ * Considering user story 2 and our emphasis on musical technicality, how accurate are our functions for mean, median, etc?
  */
 // Import functions
 import React, { useState } from "react";
+import Stats from '../src/components/Stats'
+import InputBar from '../src/components/InputBar'
+import SongBox from '../src/components/SongBox'
+
 
 const setTitle = useState([]);
 const setArtist = useState([]);
@@ -24,19 +29,19 @@ const audioFeaturesMock = { feature1: 0.5, feature2: 0.7 };
 console.log('Testing handleAddSong');
 
 // Clearing Recommendations
-console.log('Test 1: Clearing Recommendations');
+console.log('Clearing Recommendations');
 let recs = ['some', 'data'];
 handleAddSong({ preventDefault: () => {} }, recs);
 console.log('Expect setrecs to be called with []:', setrecs.mock.calls);
 
 // Adding a Song with Valid Data
-console.log('Test 2: Adding a Song with Valid Data');
+console.log('Adding a Song with Valid Data');
 getTrackInfo.mockResolvedValue(trackInfoMock);
 await handleAddSong({ preventDefault: () => {} }, [], 'Sucker', 'Jonas Brothers');
 console.log('Expect onAddSong, setTitle, setArtist to be called');
 
 // Error Handling in getTrackInfo
-console.log('Test 3: Error Handling in getTrackInfo');
+console.log('Error Handling in getTrackInfo');
 getTrackInfo.mockRejectedValue(new Error('Error'));
 await handleAddSong({ preventDefault: () => {} }, [], 'invisible string', 'Taylor Swift');
 console.log('Expect error handling logic for getTrackInfo');
@@ -49,13 +54,13 @@ console.log('Test 1: Update Songs List');
 const newSong = { id: '1', name: 'New Song' };
 const songs = [{ id: '2', name: 'Old Song' }];
 // Changed from addSong to onAddSong
-addSong(newSong, songs, /* other required params */);
+addSong(newSong, songs);
 console.log('Expect setSongs and onSongsUpdate to be called with new list');
 
 // Audio Features Handling
 console.log('Test 2: Audio Features Handling');
 getAudioFeatures.mockResolvedValue(audioFeaturesMock);
-await addSong(newSong, songs, /* other required params */);
+await addSong(newSong, songs);
 console.log('Expect audio features to be processed');
 
 // Error Handling in getAudioFeatures
@@ -64,7 +69,7 @@ getAudioFeatures.mockRejectedValue(new Error('Error'));
 await addSong(newSong, songs, /* other required params */);
 console.log('Expect error handling logic for getAudioFeatures');
 
-/* this calculates the average of the current songs that the user put in */
+/* This calculates the average of the current songs that the user put in */
 const calculateAverage = (songlistdata) => {
     const length = songlistdata.length;
     let sum1 = 0;
@@ -96,7 +101,7 @@ const newSongData1 = {
 
   const listsong = [newSongData1, newSongData2, newSongData3]
 
-// Testing whether the average function works
+// Testing whether the average function works in STATS
 
 function assert(condition, message) {
     if (!condition) {
@@ -107,3 +112,17 @@ function assert(condition, message) {
 let avg = calculateAverage(listsong)
 assert(avg[0] == 14, "correct");
 assert(avg[0] == 9, "correct");
+
+// More mock data for testing STATS functions
+const mockData = {
+    att1: 'Attribute 1',
+    att2: 'Attribute 2',
+    mean1: 10,
+    median1: 5,
+    mean2: 20,
+    median2: 15,
+  };
+  
+
+
+
